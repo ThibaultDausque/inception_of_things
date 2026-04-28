a7f14597775387eebd24e7951ebdc5f266456b9f
Anthonin <roulleau.anthonin@orange.fr>
2026-04-28 17:23:17 +0200

    chore(vagrant): update Vagrantfile and scripts

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update -y
+sudo apt-get install curl -y
+
 curl -sfL https://get.k3s.io | sh -
 
 sudo mkdir -p ~/.kube
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-MASTER_IP=$1
+sudo apt-get update -y
+sudo apt-get install curl -y
 
-TOKEN=$(cat /vagrant/token)
-
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/token) sh -
