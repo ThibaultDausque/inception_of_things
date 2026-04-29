0196b3af0be883b38c5d39135ad336e4ec17e0ef
frilas <roulleau.anthonin@orange.fr>
2026-04-29 12:02:02 +0200

    chore(provisioning): update Vagrantfile and shell scripts

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -5,8 +5,10 @@ sudo apt-get install curl -y
 
 curl -sfL https://get.k3s.io | sh -
 
-sudo mkdir -p ~/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-sudo chown -R vagrant:vagrant ~/.kube/config
+sudo mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo sed -i 's/127.0.0.1/192.168.56.110/' /home/vagrant/.kube/config
+sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
 
+mkdir -p /home/vagrant
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -3,4 +3,9 @@
 sudo apt-get update -y
 sudo apt-get install curl -y
 
+while [ ! -f /vagrant/token ]; do
+	echo "waiting for token..."
+	sleep 2
+done
+
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/token) sh -
