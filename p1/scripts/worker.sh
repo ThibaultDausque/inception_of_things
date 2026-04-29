42da2089fe2c56b9854e7a1f7108e923057e4072
frilas <roulleau.anthonin@orange.fr>
2026-04-29 18:09:29 +0200

    chore(vagrant): update Vagrantfile configuration

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -5,7 +5,7 @@ sudo apt-get install curl -y
 
 while [ ! -f /vagrant/token ]; do
 	echo "waiting for token..."
-	sleep 2
+	sleep 5
 done
 
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/token) sh -
