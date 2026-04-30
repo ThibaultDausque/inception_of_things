472f1cbbfe9c6447951d6dce623a5c7321b85558
frilas <roulleau.anthonin@orange.fr>
2026-04-30 17:05:05 +0200

    chore(vagrant): change provider and update master/worker scripts

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install curl -y
-sudo apt-get install  python3 -y
-
 curl -sfL https://get.k3s.io | sh -
 
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo sed -i 's/127.0.0.1/192.168.56.110/' /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
 
-mkdir -p /home/vagrant
-sudo cat /var/lib/rancher/k3s/server/node-token > /home/vagrant/token
-sudo chown vagrant:vagrant /home/vagrant/token
+TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 
-cd /home/vagrant
-python3 -m http.server 8080 &
+echo $TOKEN > /vagrant/token
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install -y curl netcat-openbsd
+MASTER_IP=$1
 
-while ! nc -z 192.168.56.110 8080; do
-  echo "waiting for master..."
-  sleep 5
-done
+TOKEN=$(cat /vagrant/token)
 
-TOKEN=$(curl -s http://192.168.56.110:8080/token)
-
-while ! nc -z 192.168.56.110 6443; do
-  echo "waiting for k3s api..."
-  sleep 5
-done
-
-curl -sfL https://get.k3s.io | \
-  K3S_URL=https://192.168.56.110:6443 \
-  K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
