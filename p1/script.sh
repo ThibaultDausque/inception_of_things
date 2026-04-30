79376219fbc4780ac5f48e77e0d4de5e3e719ced
root <root@tdausque.myguest.virtualbox.org>
2026-04-30 16:41:52 +0200

    feat(Server): add kubectl and k3s installation scripts

--- p1/script.sh
+++ p1/script.sh
@@ -5,4 +5,11 @@ set -e
 apt-get update
 apt-get install -y ca-certificates curl gnupg git
 
+curl -sfL https://get.k3s.io | sh -
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
 # install kubernets
