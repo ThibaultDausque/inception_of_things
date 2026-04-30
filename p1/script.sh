9d8672567b1b26da73ce42664ccabdce6ee0ecea
root <root@tdausque.myguest.virtualbox.org>
2026-04-30 17:17:31 +0200

    fix(Server): add systemd to Vagrant vm

--- p1/script.sh
+++ p1/script.sh
@@ -3,7 +3,7 @@
 set -e
 
 apt-get update
-apt-get install -y ca-certificates curl gnupg git
+apt-get install -y ca-certificates curl gnupg git systemd
 
 curl -sfL https://get.k3s.io | sh -
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
