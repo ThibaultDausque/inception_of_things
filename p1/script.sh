7dbf4c781ea0070abb86b8eb192e9b8cd12f8906
root <root@tdausque.myguest.virtualbox.org>
2026-05-05 14:19:07 +0200

    feat(Server): k3s connected

--- p1/script.sh
+++ p1/script.sh
@@ -4,12 +4,14 @@ set -e
 
 IP=$1
 
-apt-get update
-apt-get install -y ca-certificates curl gnupg git systemd net-tools
+sudo apt-get update
+sudo apt-get install -y iptables
 
-sudo curl -sfL https://get.k3s.io | K3S_URL=https://$(IP):6443 K3S_KUBECONFIG_MODE=644  sh - --bind-address=192.168.56.110
+curl -sfL https://get.k3s.io |sudo sh -s - server \
+	--node-ip=192.168.56.110
 
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
 sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
+sudo echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
 # token ?
