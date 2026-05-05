fb2691224572373aabfd25578f415d3e865d8bb0
frilas <roulleau.anthonin@orange.fr>
2026-05-05 18:13:29 +0200

    feat(p2): start implementation of part 2

---
+++ p2/scripts/master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y iptables
+
+curl -sfL https://get.k3s.io | sh -s - server --node-ip=192.168.56.110
+
+sudo mkdir -p /home/vagrant/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+sudo chown vagrant:vagrant /home/vagrant/.kube/config
+echo 'export KUBECONFIG=/home/vagrant/.kube/config' >> /home/vagrant/.bashrc
+
+TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+
+echo $TOKEN > /vagrant/token
