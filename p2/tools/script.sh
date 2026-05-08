ad70e50c774825002d6bf24310cf4dc7685ee60b
root <thi.dausque@gmail.com>
2026-05-08 09:01:31 -0400

    feat(p2_tdausque): app1 deployment

--- p2/tools/script.sh
+++ p2/tools/script.sh
@@ -11,5 +11,3 @@ curl -sfL https://get.k3s.io | K3S_URL=https://$SERV_IP:6443 sh -s - server \
 mkdir -p ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
-
-kubectl apply -f /vagrant/tools/deployment.yaml
