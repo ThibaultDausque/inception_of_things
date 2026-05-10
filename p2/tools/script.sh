cca053609e171a02fcd2fd848a6457c3d327dd41
root <thi.dausque@gmail.com>
2026-05-10 08:17:53 -0400

    feat(p2_tdausque): add app1, app2, app3

--- p2/tools/script.sh
+++ p2/tools/script.sh
@@ -6,7 +6,8 @@ sudo apt-get install -y
 sudo apt-get update -y
 
 curl -sfL https://get.k3s.io | K3S_URL=https://$SERV_IP:6443 sh -s - server \
-	--write-kubeconfig-mode 0644
+	--write-kubeconfig-mode 0644 \
+	--node-ip 192.168.56.110
 
 mkdir -p ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
