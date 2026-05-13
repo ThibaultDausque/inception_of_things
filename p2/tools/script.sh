eb536fd814c2c756db18595b4eb8122f92851bb2
root <root@42vm.myguest.virtualbox.org>
2026-05-13 14:07:00 +0200

    fix(p3_tdausque): change image to kubernetes one

--- p2/tools/script.sh
+++ p2/tools/script.sh
@@ -9,10 +9,6 @@ curl -sfL https://get.k3s.io | K3S_URL=https://$SERV_IP:6443 sh -s - server \
 	--write-kubeconfig-mode 0644 \
 	--node-ip 192.168.56.110
 
-echo "192.168.56.110 app1" >> /etc/hosts
-echo "192.168.56.110 app2" >> /etc/hosts
-echo "192.168.56.110 app3" >> /etc/hosts
-
 mkdir -p ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
