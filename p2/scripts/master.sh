a4f0b688bb3dce6882c5b0a0ee79c9f895556c3e
frilas <roulleau.anthonin@orange.fr>
2026-05-13 10:10:29 +0200

    fix(p2): update manifests to deploy hello-kubernetes successfully with app2 only

--- p2/scripts/master.sh
+++ p2/scripts/master.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y iptables
 
-curl -sfL https://get.k3s.io | sh -s - server --node-ip=192.168.56.110
+curl -sfL https://get.k3s.io | sh -s - server
 
 sudo mkdir -p /home/vagrant/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
@@ -13,6 +13,12 @@ echo 'export KUBECONFIG=/home/vagrant/.kube/config' >> /home/vagrant/.bashrc
 TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 
 echo $TOKEN > /vagrant/token
+
+echo "waiting for k3s API"
+until kubectl get nodes >/dev/null 2>&1
+do
+    sleep 5
+done
 
 kubectl apply -f /vagrant/deployment.yaml
 kubectl apply -f /vagrant/service.yaml
