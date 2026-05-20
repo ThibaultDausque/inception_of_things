6ad234eed8be79b9228b61fc7a6accf6e3d8b19e
frilas <roulleau.anthonin@orange.fr>
2026-05-20 11:35:47 +0200

    feat(setup): add script to install docker, k3d and kubectl

---
+++ p3/scripts/install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+#Install Docker
+if command -v docker &> /dev/null; then
+	echo "Docker is already installed"
+else
+	echo "Installing Docker"
+	sudo apt update -y
+	sudo apt install ca-certificates curl -y
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+	sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+	sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+	sudo usermod -aG docker $USER
+fi
+
+#Install k3d
+if command -v k3d &> /dev/null; then
+	echo "k3d is already installed"
+else
+	echo "Installing k3d"
+	curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+fi
+
+#Install kubectl
+if command -v kubectl &> /dev/null; then
+	echo "Kubectl is already installed"
+else
+	echo "Installing kubectl"
+	VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+	
+	curl -LO "https://dl.k8s.io/release/VERSION/bin/linux/amd64/kubectl"
+	curl -LO "https://dl.k8s.io/release/VERSION/bin/linux/amd64/kubectl.sha256"
+	
+	echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+	
+	chmod +x kubectl
+	sudo mv kubectl /usr/local/bin/
+fi
