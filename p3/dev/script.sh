aecce3044befd6cfa186b28f9531f6b7caf756fa
Thibault <thibault@thibaults-air.home>
2026-05-24 18:41:48 +0200

    fix(p3_tdausque): fix argocd configuration

--- p3/dev/script.sh
+++ p3/dev/script.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-kubectl create ns dev
+k3d cluster create p3-dev --config ../k3d/conf.yaml
+
 kubectl apply -n dev --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl apply -f application.yaml -n dev
